501f67560b8604bc2a616f3ce37b20f5033ffb0d
Janith Chirantha <jccandro@gmail.com>
2018-06-13 05:25:44 +0530

    Navigation bar

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-class Home extends Component{
-    render(){
-        return(
+class Login extends Component {
+    render() {
+        return (
             <div></div>
         )
     }
 }
 
-export default Home;
+export default Login;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from  './Home';
 import About from './About';
+import Login from './Login';
 
 const Main = () => (
     <main>
         <Switch>
             <Route exact path='/' component= { Home } />
             <Route exact path='/about' component= { About } />
+            <Route exact path='/login' component= { Login } />
         </Switch>
     </main>
 )
